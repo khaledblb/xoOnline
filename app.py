ee80110ed00b0f93c07bd5af37141d1945f9bc12
khaledblb <khaledblb@gmail.com>
2024-06-04 16:08:05 +0300

    update app file

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def play():
     return render_template('result.html', board=board, winner=winner)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
